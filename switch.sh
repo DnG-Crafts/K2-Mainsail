5a6f156642ae5851328f98619e1e6ec8366d0b99
DnG-Crafts <140936256+DnG-Crafts@users.noreply.github.com>
2025-01-07 13:04:00 +0800

    Initial

--- switch.sh
+++ switch.sh
@@ -14,6 +14,32 @@ break;;
 [2]* ) 
 echo -e '\033[32mUsing Mainsail as the Web UI\r\n\033[0m'
 sed -i 's^root /usr/share/fluidd^root /usr/share/mainsail^g' /etc/nginx/nginx.conf
+if [ ! -e '/usr/share/moonraker/utils' ]; then
+while true; do
+read -p "$(printf '\r\n\r\n\033[33mThe default Moonraker is being used, the camera will not be available with this version\r\nWould you like to use the version that enables the camera?\r\n\r\n\033[36m(Y|N)?: \033[0m')" res
+case $res in
+[Yy]* ) 
+if [[ $(sh -c 'exec /etc/init.d/moonraker status') == *"running"* ]]; then
+sh -c 'exec /etc/init.d/moonraker stop'
+fi
+if [ ! -e '/usr/share/moonraker_backup' ]; then
+echo "Backup original 'moonraker' to 'moonraker_backup'"
+mv /usr/share/moonraker /usr/share/moonraker_backup
+fi
+if [ -e '/usr/share/moonraker' ]; then
+rm -r /usr/share/moonraker
+fi
+echo -e '\033[32mInstalling Moonraker\033[0m'
+cp -r ~/K2-Mainsail-main/moonraker /usr/share/moonraker
+sh -c 'exec /etc/init.d/moonraker start'
+break;;
+[Nn]* ) 
+echo -e '\033[35mThe current Moonraker install has not been changed\033[0m'
+break;;
+* ) echo -e '\033[31mPlease answer Y or N\033[0m';;
+esac
+done
+fi
 break;;
 * ) echo -e '\033[31mPlease select 1 or 2\033[0m';;
 esac
